c86894b921b27c30cf707952162f43a01cfc5260
alenyu6 <lenyu@ucdavis.edu>
2017-12-10 00:01:59 -0800

    No longer immutable

--- cumul_sum.jl
+++ cumul_sum.jl
@@ -5,12 +5,10 @@ num_cores = Sys.CPU_CORES;
 if nprocs()<num_cores
   addprocs(num_cores-1)
 end
-  
 A = SharedArray{Int64}(N);
 A = rand(1:32, N);
 B = SharedArray{Int64}(num_cores);
 C = Array{Array{Int64}}(num_cores);
- 
 @everywhere function cumul_chunk(array, max_array, node_number)
 	   a = accumulate(+, array)
 	   max_array[node_number] = maximum(a)
@@ -21,19 +19,20 @@ len = length(A);
 per_node = floor(Int, len / num_cores);
 
 @sync for idx = 1:num_cores
-    @async C[idx] = remotecall_fetch(cumul_chunk,idx,A[(idx - 1)*per_node + 1: per_node*idx],B,idx);	
+    @async A[(idx - 1)*per_node + 1: per_node*idx] = remotecall_fetch(cumul_chunk,idx,A[(idx - 1)*per_node + 1: per_node*idx],B,idx);	
 end
 
 @sync for idx = 2:num_cores
-    @async C[idx] = remotecall_fetch( broadcast,idx, +, C[idx] ,sum(B[1:idx-1]) );	
+    @async A[(idx - 1)*per_node + 1: per_node*idx] = remotecall_fetch( broadcast,idx, +, A[(idx - 1)*per_node + 1: per_node*idx] ,sum(B[1:idx-1]) );	
 end
 
-
-w = collect(Iterators.flatten(C));
+A
 
 end
 
+#compile
 start(10)
 
+#run
 @time A = start(parse(Int64,ARGS[1]))
-println(A)
+#println(A)
