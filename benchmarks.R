2459afaea8bce0215024df532927019ad977dd33
Devon Johnson <dnrjohnson@ucdavis.edu>
2017-12-10 05:15:46 -0800

    Better Graphs

--- benchmarks.R
+++ benchmarks.R
@@ -6,30 +6,27 @@ maxV = read.table("output.txt", header=F)
 minV = read.table("output.txt", header=F)
 for(i in 1:100) {
 	tests[[i]] <- read.table("output.txt", header=F)
-	average <- (average + tests[[i]])/2
+	average <- (average + tests[[i]])
 	for(j in 1:7) {
+		maxV[j,3] <- max(maxV[j,3],tests[[i]][j,3])
 		maxV[j,2] <- max(maxV[j,2],tests[[i]][j,2])
-		maxV[j,1] <- max(maxV[j,1],tests[[i]][j,1])
+		minV[j,3] <- min(minV[j,3],tests[[i]][j,3])
 		minV[j,2] <- min(minV[j,2],tests[[i]][j,2])
-		minV[j,1] <- min(minV[j,1],tests[[i]][j,1])
-	}	
+	}
 	system("rm output.txt")
 	system("/bin/bash genData.sh")
 	print(average)
 }
+average <- average/100
 system("rm output.txt")
 
-#datam <- read.table("mpioutput.txt".header=F)
-plot <- ggplot(average, aes(x = average[,1])) 
-+ geom_point(tests[[]][]) 
-+ geom_line(aes(y = average[,2], color = 'red')) 
-+ geom_line(aes(y = average[,3], color = 'blue' )) 
-+ geom_line(aes(y = maxV[,2], color = 'green')) 
-+ geom_line(aes(y = maxV[,3], color = 'turqoise' )) 
-+ geom_line(aes(y = minV[,2], color = 'violet')) + 
-+ geom_line(aes(y = minV[,3], color = 'maroon' )) + scale_x_log10()
 
-for(i in 1:100) {
-	geom_point(data = tests[[i]], aes(y = tests[[i]][,2],color = 'lavendar'))
-	geom_point(data = tests[[i]], aes(y = tests[[i]][,3],color = 'lemon'))
-}
+plot <- ggplot(average, aes(x = average[,1])) +
+ geom_line(aes(y = average[,2], colour = 'Average')) +
+ geom_line(aes(y = average[,3], colour = 'Average' )) +
+ geom_line(aes(y = maxV[,2], colour = 'Max')) +
+ geom_line(aes(y = maxV[,3], colour = 'Max' )) +
+ geom_line(aes(y = minV[,2], colour = 'Min')) +
+ geom_line(aes(y = minV[,3], colour = 'Min' )) + 
+ labs(size="Values", x = "N", y = "Seconds", title = "Time tests of Julia vs MPI Code on a local machine") + scale_x_log10()
+
